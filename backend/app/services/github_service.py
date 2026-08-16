f03a5c6f270f2ee62a8711e80ff2d8e3e8adbd07
amaan <197904711+AMAN-X12@users.noreply.github.com>
2026-08-17 01:47:28 +0500

    installation token creation and tested

--- backend/app/services/github_service.py
+++ backend/app/services/github_service.py
@@ -1,6 +1,7 @@
 import time 
 import logging 
 import jwt
+import httpx
 
 def generate_JWS_Token(appID, privateKey):
     timeNow= int (time.time())
@@ -16,4 +17,20 @@ def generate_JWS_Token(appID, privateKey):
         payload,
         privateKey,
         algorithm="RS256")
+
+async def get_installation_token(installationID, jwtToken):
+    url = f"https://api.github.com/app/installations/{installationID}/access_tokens"
+    headers = {
+             "Authorization": f"Bearer {jwtToken}",
+             "Accept" : "application/vnd.github+json",
+             "X-GitHub-Api-Version":"2026-03-10"
+        }        
+    async with httpx.AsyncClient() as client :
+            response = await client.post(
+                url,
+                headers=headers
+            )
+    data = response.json()
+    installationToken = data["token"]  
+    return installationToken 
     
